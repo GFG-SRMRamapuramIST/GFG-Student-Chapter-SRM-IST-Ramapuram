dce4b752c0f957e17d283f523458f2b40bbd233d
AMISHA_KUMARI <amishak9824@gmail.com>
2024-12-25 23:28:13 +0530

    Testimonial section added

---
+++ client/src/Utility/Quote.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BsChatLeftQuoteFill } from "react-icons/bs";
+
+const quote=() => {
+  return (
+    <div>
+      <BsChatLeftQuoteFill size={40} color="blue" />
+    </div>
+  );
+};
+
+export default quote;
