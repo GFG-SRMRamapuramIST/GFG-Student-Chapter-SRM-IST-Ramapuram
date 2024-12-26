57b10001b29bcd539b157bcf2a6dcec9b66a28b6
Sai Prashanth <sai@192.168.1.6>
2024-12-26 09:33:29 +0530

    Added Subheading Component

--- client/src/Components/SubHeading/SubHeading.jsx
+++ client/src/Components/SubHeading/SubHeading.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+
 const SubHeading = () => {
-  return <div>Sub Heading to be made by ~Sai Prashanth</div>;
+  return (
+    <div className="my-40 p-6 bg-white">
+      <h1 className="text-2xl md:text-3xl font-semibold text-center">
+        Identify relevant leads and find their contact details.{" "}
+        <span className="text-green-600">In seconds.</span>
+      </h1>
+      <p className="mt-4 text-center text-gray-600 max-w-2xl mx-auto">
+        Hunter uses a combination of proprietary technology and artificial
+        intelligence to find, verify, and enrich contact details.
+      </p>
+    </div>
+  );
 };
 
 export default SubHeading;
