9a4b335fec12b4c8b7b13bb3dd5aa0c488352024
JeyasuryaUR <jeyasurya0206@gmail.com>
2024-12-27 18:11:00 +0530

    Added Responsiveness for MembersSection.

--- client/src/Components/MembersSection/MembersSection.jsx
+++ client/src/Components/MembersSection/MembersSection.jsx
@@ -10,8 +10,8 @@ const MembersSection = ({ title, caption, description, members }) => {
   useScrollEffect(containerRef, membersMemo);
 
   return (
-    <GridBackdropDiv className="flex justify-center items-center mt-12 mx-24 px-12 border-2 rounded-lg bg-[#FCFCFD] text-black gap-4 relative">
-      <div className="w-1/2 flex flex-col justify-center pr-12">
+    <GridBackdropDiv className="flex flex-col md:flex-row justify-center items-center mt-12 mx-24 px-6 md:px-12 max-md:py-12 border-2 rounded-lg bg-[#FCFCFD] text-black gap-4 relative">
+      <div className="md:w-1/2 flex flex-col justify-center max-md:items-center max-md:text-center md:pr-12">
         <div className="flex flex-col w-fit mb-2">
           <h3 className="text-3xl font-bold mb-1 uppercase text-gray-700">
             {title}
@@ -26,7 +26,7 @@ const MembersSection = ({ title, caption, description, members }) => {
         </p>
       </div>
       <div
-        className="relative flex flex-col pb-4 overflow-y-scroll no-scrollbar w-1/2 max-h-80"
+        className="relative flex max-md:w-full flex-col pb-4 overflow-y-scroll no-scrollbar md:w-1/2 max-h-80"
         ref={containerRef}
       >
         <MemberCard
