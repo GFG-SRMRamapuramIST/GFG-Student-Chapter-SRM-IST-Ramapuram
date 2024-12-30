1bcdb6a991cb48935e63d6e814ffb39b5341dce8
AMISHA_KUMARI <amishak9824@gmail.com>
2024-12-30 20:06:34 +0530

    404 Error Page added

--- client/src/Pages/PageNotFound.jsx
+++ client/src/Pages/PageNotFound.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import DinoImage from "../assets/icons/gfg Dino.png"; 
 
 const PageNotFound = () => {
-  return <div>404 Error</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white p-4">
+      <div className="text-center max-w-3xl w-full">
+        <img 
+          src={DinoImage} 
+          alt="Cute dinosaur" 
+          className="w-[500px] h-[300px] mx-auto my-6" 
+        />
+        
+        <p className="text-gray-800 text-lg md:text-xl mb-8">
+          "Oh, it's you again... 😉"
+          <br />
+          Why are you back here? Shoo shoo, go fix it already!
+        </p>
+        
+        <a 
+          href="/" 
+          className="bg-black text-white px-6 py-3 rounded inline-block text-base"
+        >
+          Go Home
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
