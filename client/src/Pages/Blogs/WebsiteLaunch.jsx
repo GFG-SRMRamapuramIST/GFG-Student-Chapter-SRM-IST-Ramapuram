d5efc4f7112ffddcb15445f88464c8bc48d6f56b
JeyasuryaUR <jeyasurya0206@gmail.com>
2025-01-02 14:25:58 +0530

    Update WebsiteLaunch component to enhance formatting and improve readability of contributor names

--- client/src/Pages/Blogs/WebsiteLaunch.jsx
+++ client/src/Pages/Blogs/WebsiteLaunch.jsx
@@ -55,10 +55,10 @@ function WebsiteLaunch() {
             website. This event marks a new chapter in our journey, providing
             members and the wider community with a centralized platform to
             explore, learn, and grow together. The website, developed under the
-            guidance of *Vishal Kumar Yadav* with contributions from *Jeysurya,
-            **Amisha Kumari, and **Sai Prashant, features sections for event
+            guidance of <strong>Vishal Kumar Yadav</strong> with contributions from <strong>Jeysurya</strong>,
+            <strong>Amisha Kumari</strong>, and <strong>Sai Prashant</strong>, features sections for event
             updates, DSA learning resources, competitive programming guides, and
-            blogs designed by **Adisesh* and *Madhav Sinha*. It also showcases
+            blogs designed by <strong>Adisesh</strong> and <strong>Madhav Sinha</strong>. It also showcases
             the profiles of our leaders and core team members, along with a
             streamlined recruitment process using Google Forms. This platform is
             a testament to the hard work and dedication of our members, aiming
