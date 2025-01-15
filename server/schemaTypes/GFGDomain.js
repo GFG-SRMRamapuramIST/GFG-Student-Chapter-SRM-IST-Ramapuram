6fe9328f41c1ed9e23a630d420eef1cf3f0522f3
AMISHA_KUMARI <amishak9824@gmail.com>
2025-01-15 21:35:05 +0530

    gfg domains and courses modified

--- server/schemaTypes/GFGDomain.js
+++ server/schemaTypes/GFGDomain.js
@@ -11,21 +11,20 @@ export default {
       options: {
         hotspot: true,
       },
-      validation: (Rule) => Rule.required().error('Domain logo is required'),
-    },
-    {
-      name: 'hashCode',
-      title: 'Hash Code',
-      type: 'string',
-      description: 'Unique hash code for the domain.',
-      validation: (Rule) => Rule.required().error('Hash code is required'),
+      fields: [
+        {
+          name: 'hashCode',
+          type: 'string',
+          title: 'Hash Code',
+          description: 'Unique hash code for the course.',
+        },
+      ],
     },
     {
       name: 'domaintitle',
       title: 'Domain Title',
       type: 'string',
       description: 'The title of the domain (e.g., Web Development).',
-      validation: (Rule) => Rule.required().error('Domain title is required'),
     },
     {
       name: 'domaintags',
@@ -33,14 +32,12 @@ export default {
       type: 'array',
       of: [{type: 'string'}],
       description: 'Tags related to the domain (e.g., Frontend, Backend).',
-      validation: (Rule) => Rule.min(1).error('At least one tag is required'),
     },
     {
       name: 'cardbgColor',
       title: 'Card Background Color',
       type: 'string',
       description: 'Background color for the card (e.g., bg-blue-50).',
-      validation: (Rule) => Rule.required().error('Background color is required'),
     },
   ],
 }
